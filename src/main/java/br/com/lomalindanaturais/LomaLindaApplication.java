9fd766d9d317380ebed6d1f8d0d9fb14245dad97
walescko <walescko@gmail.com>
2021-11-20 08:15:13 -0300

    Iniciando a API para controle de estoque

---
+++ src/main/java/br/com/lomalindanaturais/LomaLindaApplication.java
@@ -0,0 +1,11 @@
+package br.com.lomalindanaturais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LomaLindaApplication {
+    public static void main(String[] args){
+        SpringApplication.run(LomaLindaApplication.class, args);
+    }
+}
