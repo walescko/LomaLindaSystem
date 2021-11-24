ab2f75c53c5523eacea82704d62049ffed96c800
Walescko <walescko@gmail.com>
2021-11-24 17:59:18 -0300

    Controller Categoria - falta os parametros

--- src/main/java/br/com/lomalindanaturais/controllers/CategoriaController.java
+++ src/main/java/br/com/lomalindanaturais/controllers/CategoriaController.java
@@ -1,2 +1,14 @@
-package br.com.lomalindanaturais.controller;public class CategoriaController {
+package br.com.lomalindanaturais.controllers;
+
+
+import br.com.lomalindanaturais.services.ProtudosService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CategoriaController {
+
+    @Autowired
+    private ProtudosService produtoservice;
+
+
+
 }
