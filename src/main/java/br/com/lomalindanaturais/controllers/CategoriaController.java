37419cb75abde060f93c546af40d9972ae61a87b
walescko <walescko@gmail.com>
2021-11-25 17:19:36 -0300

    Add functions and methods of class

--- src/main/java/br/com/lomalindanaturais/controllers/CategoriaController.java
+++ src/main/java/br/com/lomalindanaturais/controllers/CategoriaController.java
@@ -1,13 +1,13 @@
 package br.com.lomalindanaturais.controllers;
 
 
-import br.com.lomalindanaturais.services.ProtudosService;
+import br.com.lomalindanaturais.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CategoriaController {
 
     @Autowired
-    private ProtudosService produtoservice;
+    private ProdutoService produtoservice;
 
 
 
